76ae8db353248a86280e992fbfbab1f577a83b22
Administrator <gitlab_admin_371d15@example.com>
2025-10-08 19:21:29 +0300

    Initial commit

---
+++ shop/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Course
+
+
+
+def index(request):
+    courses = Course.objects.all()
+    return render(request, 'courses.html', {'courses': courses})
